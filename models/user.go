4a39de3bee17bc03a236134436b153033ef41965
Vikash Parashar <vikash@nexg.tech>
2023-10-25 00:44:26 +0530

    fixing required UI components

--- models/user.go
+++ models/user.go
@@ -4,9 +4,28 @@ const (
 	UserRoleAdmin   = "admin"
 	UserRoleGeneral = "general"
 )
+const (
+	DeviceTypeServer        = "Server"
+	DeviceTypeObjectStorage = "Object Storage"
+	DeviceTypeSwitch        = "Switch"
+	DeviceTypeSANSwitch     = "SAN Switch"
+)
+const (
+	DeviceMakeOracle  = "Oracle"
+	DeviceMakeHitachi = "Hitachi"
+	DeviceMakeCisco   = "Cisco"
+	DeviceMakeBrocade = "Brocade"
+)
+
+const (
+	DeviceModelT84        = "T8-4"
+	DeviceModelHCPG10     = "HCP-G10"
+	DeviceModelNEXUS93108 = "NEXUS-93108"
+	DeviceModel6520       = "6520"
+)
 
 type User struct {
-	ID        int    `json:"id"`
+	Id        int    `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Username  string `json:"username"`
